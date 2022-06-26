a079e74149a48ce6472a8adff3d4ca8888367b21
max <github.com@maxos.email>
2022-06-26 12:39:10 +0800

    Initial Commit

---
+++ Flash-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flash-UIKit
+//
+//  Created by Max Zhang on 2022/6/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
